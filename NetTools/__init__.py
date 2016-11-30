058e8b63e9f1aa8509cd938d5701bbbded2cabe4
antonio <tgq.spm@gmail.com>
2016-11-30 23:20:08 +0100

    Improving coverage by inspecting version and release information.

--- NetTools/__init__.py
+++ NetTools/__init__.py
@@ -6,9 +6,13 @@ Module which groups utilities to work with networks.
 
 """
 
+import version
+import release
+
 from tests import test_netsym, test_netstructure
 ## Not inform about warnings
 import warnings
+
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
 warnings.simplefilter("ignore")
